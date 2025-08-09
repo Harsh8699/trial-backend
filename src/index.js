d971cb226c912977cdf62686155fc2032942c935
Harsh Singh <harshsingh83252@gmail.com>
2025-08-10 00:25:40 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -11,3 +11,13 @@ connectDB()
 
 
 
+.then(() => {
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`Server is running at port ${process.env.PORT}`);
+        
+    });
+})
+.catch((err) => {
+    console.log("Mongdb connection failed",err);
+    
+})
